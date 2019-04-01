1d7d614628795973a39a8ee01cb7c053d3f99f71
Nwe Ni Wah <nweniwah4694@gmail.com>
2019-04-01 16:50:04 +0630

    Updates

---
+++ precache-manifest.2f16e09ada8612c8399907fdb515444a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nweniwah4694/react-movies-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4366970be6e17747f364",
+    "url": "/nweniwah4694/react-movies-pages/static/js/runtime~main.cc4b17e2.js"
+  },
+  {
+    "revision": "b4d078fb6d31514bfd3d",
+    "url": "/nweniwah4694/react-movies-pages/static/js/main.0164ef95.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/nweniwah4694/react-movies-pages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b4d078fb6d31514bfd3d",
+    "url": "/nweniwah4694/react-movies-pages/static/css/main.f020f4d5.chunk.css"
+  },
+  {
+    "revision": "63bdfed6776ef18182eda78acaaaed2b",
+    "url": "/nweniwah4694/react-movies-pages/index.html"
+  }
+];
